30261551288789523a83b8560c4f793f42324f16
ammduncan <ammielgyanyawson@gmail.com>
2022-09-09 15:56:25 +0000

    fix 7

--- more_mod_handlers.c
+++ more_mod_handlers.c
@@ -12,5 +12,10 @@ void div_handler(stack_t **stack, unsigned int line)
 		fprintf(stderr, "L%d: can't div, stack too short\n", line);
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line);
+		exit(EXIT_FAILURE);
+	}
 	div(stack);
 }
