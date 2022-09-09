d9542294249a56e393c1612d798eeeb1247d94be
ammduncan <ammielgyanyawson@gmail.com>
2022-09-09 16:10:07 +0000

    fix 9

--- more_mod_handlers.c
+++ more_mod_handlers.c
@@ -47,5 +47,10 @@ void mod_handler(_stack_t **stack, unsigned int line)
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line);
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line);
+		exit(EXIT_FAILURE);
+	}
 	mod_stack(stack);
 }
